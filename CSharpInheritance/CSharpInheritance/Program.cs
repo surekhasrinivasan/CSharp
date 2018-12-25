81a8df8708c4306ecfaa93f5a447dd1bca60bf7f
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-25 07:22:43 -0500

    CSharpInheritance - Created Class for three Objects Man, Kart and Alien

---
+++ CSharpInheritance/CSharpInheritance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInheritance
+{
+    // Program to show Inheritance and Hierarchy
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var man = new Man();
+            var kart = new Kart();
+            var alien = new Alien();            
+        }
+    }
+}
+
+// Man class
+public class Man
+{
+    public Man(){
+    }
+    public bool moves { get; set; } 
+    public string name { get; set; }
+    public string color { get; set; }
+}       
+
+// Kart class
+public class Kart
+{
+    public Kart(){
+    }
+    public bool moves { get; set; }
+    public string manufacturer { get; set; }
+    public string color { get; set; }
+}
+
+// Alien class 
+public class Alien 
+{
+    public Alien(){
+    }
+    public bool moves { get; set; }
+    public string planet { get; set; }
+    public string color { get; set; }
+}
+
+
+
+
+
