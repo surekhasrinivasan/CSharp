b64a6092beaf4c1c6666cfae48891d6288cafb33
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-27 07:34:41 -0500

    CSharpPolymorphism - Initial commit

---
+++ CSharpPolymorphism/CSharpPolymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPolymorphism
+{
+    class Program
+    {
+        // Example for Polymorphism
+        static void Main(string[] args)
+        {
+            var worker = new FactoryWorker();
+            worker.Count(5);
+            worker.Count(6);
+        }
+    }
+
+    public class FactoryWorker
+    {
+        public FactoryWorker()
+        {
+        }
+
+        public void Count(int number)
+        {
+            if (Total == null)
+                Total = 0;
+
+            Total = Total + number;
+            Console.WriteLine(Total.ToString());
+        }
+
+        // Class variable
+        public int Total { get; set; }
+    }
+}
