7d3af58d28743fd82ae0d01b506c20c6bb8f2817
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-28 06:21:10 -0500

    Example to demo encapsulation - created a method to add numbers

--- CSharpEncapsulation/CSharpEncapsulation/Program.cs
+++ CSharpEncapsulation/CSharpEncapsulation/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace CSharpEncapsulation
 {
+    // Example for encapsulation
     class Program
     {
         static void Main(string[] args)
         {
+            var worker = new FactoryWorker();
+            var result = worker.addNumbers(5, 3);
+            Console.WriteLine(result);
         }
+    }
+
+    public class FactoryWorker
+    {
+        public FactoryWorker(){
+        }
+
+        // Method to add numbers 
+        public int addNumbers(int num1, int num2)
+        {
+            var result = num1 + num2;
+            return result;
+        } 
     }
 }
