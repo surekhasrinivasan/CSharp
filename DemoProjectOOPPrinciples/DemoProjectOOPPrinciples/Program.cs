02824c9fcb96bafa02aaf2f0c98249470a914a5f
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-28 07:31:22 -0500

    Created class Car, car1 object, and SaloonCar class which inherits Car class

--- DemoProjectOOPPrinciples/DemoProjectOOPPrinciples/Program.cs
+++ DemoProjectOOPPrinciples/DemoProjectOOPPrinciples/Program.cs
@@ -11,6 +11,36 @@ namespace DemoProjectOOPPrinciples
     {
         static void Main(string[] args)
         {
+            var car1 = new Car();
+            car1.color = "Red";
+            car1.manufacturer = "Toyota";
+
+            Console.WriteLine("Car1 is " + car1.color + " in color and the make is " + car1.manufacturer);
+
+            var saloonCar1 = new SaloonCar();
+            saloonCar1.numberOfSeats = 5;
+
+            Console.WriteLine("Car1 is " + car1.color + " in color, the make is " + car1.manufacturer + " and number of seats is " + saloonCar1.numberOfSeats);
+
+
         }
+    }
+
+    public class Car
+    {
+        public Car(){
+        }
+
+        public string color { get; set; }
+        public string manufacturer { get; set; }
+    }
+
+    public class SaloonCar : Car
+    {
+        public SaloonCar()
+        {
+        }
+
+        public int numberOfSeats { get; set; }
     }
 }
