67859c902d0caa6fdbb564d359faa83cb42b5f25
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-28 07:10:33 -0500

    Demo Project - initial commit

---
+++ DemoProjectOOPPrinciples/DemoProjectOOPPrinciples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectOOPPrinciples
+{
+    // Demo program to show class, objects, OOP principles like Encapsulation, Inheritance and Polymorphism    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
