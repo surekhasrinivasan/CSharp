b2c76025a30344e31d1c97168382d0898311d96c
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-29 06:40:36 -0500

    Demo OOP project showing encapsulation,inheritance and polymorphism completed

--- DemoProjectOOPPrinciples/DemoProjectOOPPrinciples/Program.cs
+++ DemoProjectOOPPrinciples/DemoProjectOOPPrinciples/Program.cs
@@ -17,12 +17,19 @@ namespace DemoProjectOOPPrinciples
 
             Console.WriteLine("Car1 is " + car1.color + " in color and the make is " + car1.manufacturer);
 
-            var saloonCar1 = new SaloonCar();
-            saloonCar1.numberOfSeats = 5;
+            var saloonCar = new SaloonCar();
+            saloonCar.numberOfSeats = 5;
 
-            Console.WriteLine("Car1 is " + car1.color + " in color, the make is " + car1.manufacturer + " and number of seats is " + saloonCar1.numberOfSeats);
+            Console.WriteLine("SaloonCar is " + car1.color + " in color, the make is " + car1.manufacturer + " and number of seats is " + saloonCar.numberOfSeats);
 
+            var saloonCar1 = new SaloonCar(2);
+            Console.WriteLine("saloonCar1 has " + saloonCar1.numberOfSeats + " seats");
 
+            var saloonCar2 = new SaloonCar(3, "Nissan");
+            Console.WriteLine("saloonCar2 has " + saloonCar2.numberOfSeats + " seats and its manufacturer is " + saloonCar2.manufacturer);
+
+            var saloonCar3 = new SaloonCar(4, "Ford", "purple");
+            Console.WriteLine("saloonCar3 has " + saloonCar3.numberOfSeats + " seats, its manufacturer is " + saloonCar3.manufacturer + " and color is " + saloonCar3.color);
         }
     }
 
@@ -35,12 +42,36 @@ namespace DemoProjectOOPPrinciples
         public string manufacturer { get; set; }
     }
 
+    // Inheritance
     public class SaloonCar : Car
     {
+        // Default constructor
         public SaloonCar()
         {
         }
 
         public int numberOfSeats { get; set; }
+
+        // Polymorphism
+
+        // Parameterized constructor
+        public SaloonCar(int numberOfSeats)
+        {
+            this.numberOfSeats = numberOfSeats;
+
+        }
+        
+        public SaloonCar(int numberOfSeats, string manufacturer)
+        {
+            this.numberOfSeats = numberOfSeats;
+            this.manufacturer = manufacturer;
+        }
+
+        public SaloonCar(int numberOfSeats, string manufacturer, string color)
+        {
+            this.numberOfSeats = numberOfSeats;
+            this.manufacturer = manufacturer;
+            this.color = color;
+        }
     }
 }
