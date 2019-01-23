a4b504ba3d76ba1a95b71d81bbb2442f81e087a6
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-23 15:38:33 -0500

    Two classes having same Work method - initial commit

---
+++ MethodOverloadingExample/MethodOverloadingExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingExample
+{
+    // Example program to show method overloading 
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void Work()
+        {
+            Console.WriteLine("It works....");
+        }
+    }
+
+    class Employee : Person
+    {
+        public double Salary { get; set; }
+        public string Designation { get; set; }
+        public void Work()
+        {
+            Console.WriteLine("It earns....");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Employee();
+            p1.Work();
+        }
+    }
+}
