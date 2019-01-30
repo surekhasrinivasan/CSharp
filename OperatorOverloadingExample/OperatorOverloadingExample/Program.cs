9f12cb16963382b7b4a5f534ada97a70c2222dfb
surekhasrinivasan <surekhasrao@gmail.com>
2019-01-30 13:35:42 -0500

    Example for OperatorOverloading - completed

--- OperatorOverloadingExample/OperatorOverloadingExample/Program.cs
+++ OperatorOverloadingExample/OperatorOverloadingExample/Program.cs
@@ -6,14 +6,7 @@ using System.Threading.Tasks;
 
 namespace OperatorOverloadingExample
 {
-    // Example Program to show Operator Overloading 
-    class program
-    {
-        static void Main(string[] args)
-        {
-
-        }
-    }
+    // Example Program to show Operator Overloading
 
     class Rect
     {
@@ -28,4 +21,27 @@ namespace OperatorOverloadingExample
             return rec;
         }
     }
+     
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Rect rec1, rec2, rec3;
+
+            rec1 = new Rect();
+            rec1.length = 3;
+            rec1.breadth = 3;
+
+            rec2 = new Rect();
+            rec2.length = 7;
+            rec2.breadth = 7;
+
+            rec3 = new Rect();
+            rec3 = rec1 + rec2;
+
+            Console.WriteLine("The length of the Rectangle 3 is: " + rec3.length);
+            Console.WriteLine("The breadth of the Rectangle 3 is: " + rec3.breadth);
+            Console.Read();
+        }
+    }    
 }
