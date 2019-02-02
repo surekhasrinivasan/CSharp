7c7faf149e2f72a6b79830ec47905a2238e4a0c8
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-01 22:39:56 -0500

    Abstract class example - initial commit

---
+++ AbstractClassExample/AbstractClassExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExample
+{
+    // Example to show Abstract class and Abstract method
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
