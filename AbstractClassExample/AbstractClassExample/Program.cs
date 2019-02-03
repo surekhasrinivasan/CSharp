fefc145c8536b64d8c2187bfb94583e4e473b8f4
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-02 23:01:36 -0500

    Abstract class is not yet implemented

--- AbstractClassExample/AbstractClassExample/Program.cs
+++ AbstractClassExample/AbstractClassExample/Program.cs
@@ -7,10 +7,61 @@ using System.Threading.Tasks;
 namespace AbstractClassExample
 {
     // Example to show Abstract class and Abstract method
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual void Work()
+        {
+            Console.WriteLine("It works...");
+        }
+    }
+
+    class Student : Person
+    {
+        public override void Work()
+        {
+            Console.WriteLine("It studies...");
+        }
+    }
+
+    class Employee : Person
+    {
+        public double Salary { get; set; }
+        public string Designation { get; set; }
+
+        public override void Work()
+        {
+            Console.WriteLine("It earns...");
+        }
+    }
+
+    class Manager  : Employee
+    {
+        public sealed override void Work()
+        {
+            Console.WriteLine("It manages a team...");
+        }
+
+    }
+
+    class BranchManager : Manager
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Person p1 = new Student();
+            p1.Work();
+            p1 = new Employee();
+            p1.Work();
+            p1 = new BranchManager();
+            p1.Work();
         }
     }
 }
