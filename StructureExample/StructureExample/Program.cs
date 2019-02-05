52436438df97bf325d3512cdc059cc80bbf996ba
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-04 22:43:24 -0500

    Structure Example - completed

--- StructureExample/StructureExample/Program.cs
+++ StructureExample/StructureExample/Program.cs
@@ -7,10 +7,28 @@ using System.Threading.Tasks;
 namespace StructureExample
 {
     // Program to demo Structure
+
+    struct Student
+    {
+        public int ID;
+        public string Name;
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
+            // Creating an object of the Student class
+            Student student;
+
+            // Defining the properties of the structure
+            student.ID = 1;
+            student.Name = "John";
+
+            // Displaying the properties of the structure
+            Console.WriteLine(student.ID);
+            Console.WriteLine(student.Name);
+            Console.Read();
         }
     }
 }
