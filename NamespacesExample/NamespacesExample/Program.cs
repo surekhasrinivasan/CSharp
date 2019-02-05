2e13ed85c791662ad5793e915ba8e897d1325bfe
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-04 22:56:53 -0500

    NamespaceExample - completed

---
+++ NamespacesExample/NamespacesExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name1
+{
+    class ClassA
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Namespace1");
+        }
+    }
+}
+namespace name2
+{
+    class ClassA
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Namespace2");
+        }
+    }
+}
+namespace NamespacesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            name1.ClassA.Display();
+            name2.ClassA.Display();
+
+        Console.Read();
+        }
+    }
+}
