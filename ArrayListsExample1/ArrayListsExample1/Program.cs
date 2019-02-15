9cb95197f25d537b01d50dd89864cd7dc1d039e7
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-14 21:14:39 -0500

    Program uses Add() method to add string elements to array list

--- ArrayListsExample1/ArrayListsExample1/Program.cs
+++ ArrayListsExample1/ArrayListsExample1/Program.cs
@@ -20,10 +20,18 @@ namespace ArrayListsExample1
             ar.Add(2);
             ar.Add(3);
 
+            // Adding string elements to the array list
+            ar.Add("Hello");
+            ar.Add("World");
+            ar.Add("Again");
+
             // Displaying the elements
             Console.WriteLine("The first element of the array is " + ar[0]);
             Console.WriteLine("The second element of the array is " + ar[1]);
             Console.WriteLine("The third element of the array is " + ar[2]);
+            Console.WriteLine("The fourth element of the array is " + ar[3]);
+            Console.WriteLine("The fifth element of the array is " + ar[4]);
+            Console.WriteLine("The sixth element of the array is " + ar[5]);
             Console.Read();
         }
     }
