777fbf1a8233b23aa2fa3a092fd67137f1ca2f67
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-10 11:16:04 -0400

    Genericmethod with type parameter - completed

--- CSharpGenerics/CSharpGenerics/Tester.cs
+++ CSharpGenerics/CSharpGenerics/Tester.cs
@@ -75,7 +75,32 @@ namespace CSharpGenerics
 
         static void Main(string[] args)
         {
+            int a, b;
+            char c, d;
 
+            a = 10;
+            b = 20;
+            c = 'I';
+            d = 'V';
+
+            // display values before swap:
+            Console.WriteLine("Int values before calling swap: ");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Character values before calling swap: ");
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+
+            // call swap
+            Swap<int>(ref a, ref b);
+            Swap<char>(ref c, ref d);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // display values after swap:
+            Console.WriteLine("Int values after calling swap: ");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Character values after calling swap: ");
+            Console.WriteLine("c = {0}, d = {1}", c, d);
         }
     }
 }
