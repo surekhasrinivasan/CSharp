51ebce42a8d16a332bf453a598890435369cfa1b
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-10 10:45:26 -0400

    Implement Swap method

--- CSharpGenerics/CSharpGenerics/Tester.cs
+++ CSharpGenerics/CSharpGenerics/Tester.cs
@@ -31,44 +31,51 @@ namespace CSharpGenerics
     //    static void Main(string[] args)
     //    {
     //        //// declaring an int array
-            //MyGenericArray<int> intArray = new MyGenericArray<int>(5);
-            //// setting values 
-            //for(int c = 0; c < 5; c++)
-            //{
-            //    intArray.setItem(c, c * 5);
-            //}
-            //// retrieving the values
-            //for(int c = 0; c < 5; c++)
-            //{
-            //    Console.Write(intArray.getItem(c) + " ");
-            //}
-            //Console.WriteLine();
+    //MyGenericArray<int> intArray = new MyGenericArray<int>(5);
+    //// setting values 
+    //for(int c = 0; c < 5; c++)
+    //{
+    //    intArray.setItem(c, c * 5);
+    //}
+    //// retrieving the values
+    //for(int c = 0; c < 5; c++)
+    //{
+    //    Console.Write(intArray.getItem(c) + " ");
+    //}
+    //Console.WriteLine();
 
-            //// delcaring a character array
-            //MyGenericArray<char> charArray = new MyGenericArray<char>(5);
-            //// setting values
-            //for(int c = 0; c < 5; c++)
-            //{
-            //    charArray.setItem(c, (char)(c + 97));
-            //}
-            //// retrieving the values
-            //for(int c = 0; c < 5; c++)
-            //{
-            //    Console.Write(charArray.getItem(c) + " ");
-            //}
-            //Console.WriteLine();
-            //Console.ReadKey();
+    //// delcaring a character array
+    //MyGenericArray<char> charArray = new MyGenericArray<char>(5);
+    //// setting values
+    //for(int c = 0; c < 5; c++)
+    //{
+    //    charArray.setItem(c, (char)(c + 97));
+    //}
+    //// retrieving the values
+    //for(int c = 0; c < 5; c++)
+    //{
+    //    Console.Write(charArray.getItem(c) + " ");
+    //}
+    //Console.WriteLine();
+    //Console.ReadKey();
 
-        //}
+    //}
     //}
 
     // A Example program to demo Generic method with type parameter
     class Program
     {
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
 
-    }
-    static void Main(string[] args)
-    {
+        static void Main(string[] args)
+        {
 
+        }
     }
 }
