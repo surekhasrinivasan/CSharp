862832d3fbed4e7f88c22ca6b206b4aecfda5d1d
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-09 22:08:53 -0500

    Created MyGenericArray class

---
+++ CSharpGenerics/CSharpGenerics/Tester.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGenerics
+{
+    // An example program to under the concept of Generics
+
+    public class MyGenericArray<T>
+    {
+        private T[] array;
+
+        public MyGenericArray(int size)
+        {
+            array = new T[size + 1];
+        }
+
+        public T getItem(int index)
+        {
+            return array[index];
+        }
+
+        public void setItem(int index, T value)
+        {
+            array[index] = value;
+        }
+    }
+
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
