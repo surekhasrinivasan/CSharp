a8803587080a9bb2da03e19d2b8cf60e95d75519
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-11 22:39:08 -0400

    Example 2 - completed

--- CSharpLinkedListExample1/CSharpLinkedListExample1/Program.cs
+++ CSharpLinkedListExample1/CSharpLinkedListExample1/Program.cs
@@ -21,6 +21,25 @@ namespace CSharpLinkedListExample1
             {
                 Console.WriteLine(element);
             }
+
+
+            // Example 2 - A linked-list is a collection type that on the surface is very similar to the List<T> type, 
+            // but the big difference is how the data is stored internally
+
+            LinkedList<string> months = new LinkedList<string>();
+
+            months.AddLast("March");
+            months.AddFirst("January");
+
+            var march = months.Find("March");
+
+            months.AddBefore(march, "February");
+            months.AddAfter(march, "April");
+
+            foreach(var month in months)
+            {
+                Console.WriteLine(month);
+            }
         }
     }
 }
