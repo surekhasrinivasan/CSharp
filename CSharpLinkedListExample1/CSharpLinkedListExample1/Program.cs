277d46ddca1dc78637be72fbd0031ce7854e164e
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-11 22:50:56 -0400

    Example 3 - completed

--- CSharpLinkedListExample1/CSharpLinkedListExample1/Program.cs
+++ CSharpLinkedListExample1/CSharpLinkedListExample1/Program.cs
@@ -17,11 +17,11 @@ namespace CSharpLinkedListExample1
             l.AddLast("Two");
             l.AddLast("Three");
 
-            foreach(var element in l)
+            foreach (var element in l)
             {
                 Console.WriteLine(element);
             }
-
+            Console.WriteLine();
 
             // Example 2 - A linked-list is a collection type that on the surface is very similar to the List<T> type, 
             // but the big difference is how the data is stored internally
@@ -36,10 +36,45 @@ namespace CSharpLinkedListExample1
             months.AddBefore(march, "February");
             months.AddAfter(march, "April");
 
-            foreach(var month in months)
+            foreach (var month in months)
             {
                 Console.WriteLine(month);
             }
+            Console.WriteLine();
+
+            // Example 3 - illustrate the LinkedList<T> class properties 
+
+            // Created LinkedList of string
+            LinkedList<string> myList = new LinkedList<string>();
+
+            // Adding Nodes in the LinkedList
+            myList.AddLast("GeeksforGeeks");
+            myList.AddLast("GFG");
+            myList.AddLast("Data structures");
+            myList.AddLast("Noida");
+
+            // Count Property
+            // To get the first Node of the LinkedList
+            if(myList.Count > 0)
+            {
+                Console.WriteLine(myList.First.Value);
+            }
+            else
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }            
+
+            // Last Property
+            // To get the last Node of the LinkedList
+            if(myList.Count > 0)
+            {
+                Console.WriteLine(myList.Last.Value);
+            }
+            else
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            Console.WriteLine();
         }
     }
 }
