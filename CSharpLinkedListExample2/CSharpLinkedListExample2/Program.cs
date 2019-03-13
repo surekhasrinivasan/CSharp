efd96cfca7b9023b2bd4cc80ca9d5f335bff18f5
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-12 21:13:44 -0400

    Implementation of simple single LinkedList - initial commit

---
+++ CSharpLinkedListExample2/CSharpLinkedListExample2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLinkedListExample2
+{
+    // simple c# program to implement Single Link List with operations AddItemStart, AddItemEnd, RemoveItemStart, RemoveItemEnd and DisplayAllItems
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
