9b40088d3d1c4b666c4b308924cfccb00657073a
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-12 21:43:14 -0400

    Created methods RemoveStart and RemoveEnd

--- CSharpLinkedListExample2/CSharpLinkedListExample2/Program.cs
+++ CSharpLinkedListExample2/CSharpLinkedListExample2/Program.cs
@@ -42,6 +42,39 @@ namespace CSharpLinkedListExample2
             current = newnode;
             counter++;
         }
+        
+        public void RemoveStart()
+        {
+            if(counter > 0)
+            {
+                head.next = head.next.next;
+                counter--;
+            }
+            else
+            {
+                Console.WriteLine("No elements exist in this linked list);
+            }
+
+        }
+
+        public void RemoveEnd()
+        {
+            if(counter > 0)
+            {
+                Node prevNode = new Node();
+                Node cur = head;
+                while(cur.next != null)
+                {
+                    prevNode = cur;
+                    cur = cur.next;
+                }
+                prevNode.next = null;
+            }
+            else
+            {
+                Console.WriteLine("No elements exist in this linked list");
+            }
+        }
 
         static void Main(string[] args)
         {
