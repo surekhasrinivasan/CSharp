166c150ba64ff1dbb22bbea73af6f9ec47ec685e
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-13 22:33:37 -0400

    Methods like InsertNext and Delete Next are coded

--- CSharpSingleLinkedList/CSharpSingleLinkedList/Program.cs
+++ CSharpSingleLinkedList/CSharpSingleLinkedList/Program.cs
@@ -8,6 +8,50 @@ namespace CSharpSingleLinkedList
 {
     // C# program to implement Singly LinkedList traversal
 
+    class SingleLinkedList
+    {
+        private int data;
+        private SingleLinkedList next;
+
+        public SingleLinkedList()
+        {
+            data = 0;
+            next = null;
+        }
+
+        public SingleLinkedList(int value)
+        {
+            data = value;
+            next = null;
+        }
+
+        public SingleLinkedList InsertNext(int value)
+        {
+            SingleLinkedList node = new SingleLinkedList(value);
+            if(this.next == null)
+            {
+                node.next = null;
+                this.next = node;
+            }
+            else
+            {
+                SingleLinkedList temp = this.next;
+                node.next = temp;
+                this.next = node;
+            }
+            return node;
+        }
+        public int DeleteNext()
+        {
+            if(next == null)         
+                return 0;
+            SingleLinkedList node = this.next;
+            this.next = this.next.next;
+            node = null;
+            return 1;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
