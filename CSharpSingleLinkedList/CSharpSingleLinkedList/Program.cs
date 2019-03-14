7ca8bf1492495e8c714c0a1bc98a0d714580b1fd
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-13 22:46:41 -0400

    Implementation of Single Linked List - completed

--- CSharpSingleLinkedList/CSharpSingleLinkedList/Program.cs
+++ CSharpSingleLinkedList/CSharpSingleLinkedList/Program.cs
@@ -50,12 +50,34 @@ namespace CSharpSingleLinkedList
             node = null;
             return 1;
         }
+
+        public void Traverse(SingleLinkedList node)
+        {
+            if (node == null)
+                node = this;
+            Console.WriteLine("Traversing Singly Linked List: ");
+            while(node != null)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            SingleLinkedList node1 = new SingleLinkedList(100);
+            SingleLinkedList node2 = node1.InsertNext(200);
+            SingleLinkedList node3 = node2.InsertNext(300);
+            SingleLinkedList node4 = node3.InsertNext(400);
+            SingleLinkedList node5 = node4.InsertNext(500);
+            node1.Traverse(null);
+            Console.WriteLine("Deleting from Linked List...");
+            node3.DeleteNext();
+            node2.Traverse(null);
+            Console.ReadLine();            
         }
     }
 }
