84e353877cfd6c15a749a4fa12863dcdf127401a
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-20 23:06:26 -0400

    LinkedList class created, also added an Append method

--- CSharpLinkedListExample3/CSharpLinkedListExample3/Program.cs
+++ CSharpLinkedListExample3/CSharpLinkedListExample3/Program.cs
@@ -18,6 +18,28 @@ namespace CSharpLinkedListExample3
         {
             public int Data = 0;
             public Node next = null;
-        }        
+        }
+
+        public static class LinkedList
+        {
+            public static void Append(ref Node head, int data)
+            {
+                if (head != null)
+                {
+                    Node current = head;
+                    while(current.next != null)
+                    {
+                        current = current.next;
+                    }
+                    current.next = new Node();
+                    current.next.Data = data;
+                }
+                else
+                {
+                    head = new Node();
+                    head.Data = data;
+                }
+            }
+        }
     }
 }
