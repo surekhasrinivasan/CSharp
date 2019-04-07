fd1ea64cfb3cb09cfd33354ef8706fd77e84941c
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-07 17:27:58 -0400

    ExampleForStaticVsInstanceConstructor - initial commit

---
+++ ExampleForStaticVsInstanceConstructor/ExampleForStaticVsInstanceConstructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleForStaticVsInstanceConstructor
+{
+    /* Static constructor is a special constructor that gets called before the first object of the 
+     * class is created. It is used to initialize any static data, or to perform a particular action 
+     * that needs performed once only.
+     * 
+     * Instance constructor is used to initialize instance data. Instance constructor is called 
+     * every time when object of class is created.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
