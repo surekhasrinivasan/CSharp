53da4261a920befeeb94c3657eac96d8fc80a7ec
surekhasrinivasan <surekhasrao@gmail.com>
2019-04-13 18:57:52 -0400

    CSharpGenericHashSetExample1 demo - initial commit

---
+++ CSharpGenericHashSetExample1/CSharpGenericHashSetExample1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGenericHashSetExample1
+{
+    /* Program creates two HashSet<T> objects and populates them with even and odd numbers. 
+     * Then a third HashSet<T> object is created from the set that contains the even numbers. 
+     * Using UnionWith method it adds the odd number set to the third set. 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
