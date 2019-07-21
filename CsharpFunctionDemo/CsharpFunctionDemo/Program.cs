2e38db03599f5ebb911e4bec74f923ba580e9823
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-21 17:20:42 -0400

    CsharpFunctionDemo - initial commit

---
+++ CsharpFunctionDemo/CsharpFunctionDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFunctionDemo
+{
+    // C# function: Using no parameter and return type
+
+    // A function that does not return any value specifies void 
+    // type as a return type.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
