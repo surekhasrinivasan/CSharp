aa20530f4e91f66bfec94afe7f586e84e8eef5b1
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-21 17:36:12 -0400

    Csharp function using parameter and return type as string

--- CsharpFunctionDemo/CsharpFunctionDemo/Program.cs
+++ CsharpFunctionDemo/CsharpFunctionDemo/Program.cs
@@ -28,18 +28,37 @@ namespace CsharpFunctionDemo
 
     // C# function: Using parameter but no return type
 
+    //class Program
+    //{
+    //    // No return type
+    //    public void Show(string message) 
+    //    {
+    //        Console.WriteLine("Hello " + message);
+    //    }
+
+    //    static void Main(string[] args)
+    //    {
+    //        Program program = new Program(); // Creating object
+    //        program.Show("Happy World!"); // calling function
+    //    }
+    //}
+
+    // C# function: Using parameter and return type
+
     class Program
     {
-        // No return type
-        public void Show(string message) 
+        // function with return type (string in this case)
+        public string Show(string message)
         {
-            Console.WriteLine("Hello " + message);
+            Console.Write("Inside Show function: ");
+            return message;
         }
 
         static void Main(string[] args)
         {
             Program program = new Program(); // Creating object
-            program.Show("Happy World!"); // calling function
+            string message = program.Show("Happy World!"); // calling function
+            Console.WriteLine("Hello " + message);
         }
     }
 }
