710a69efa9e4fadad26f970ce06a6d737c113aa7
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-22 21:41:57 -0400

    Example for Call By Value - completed

--- ExampleForCallByValueAndCallByRef/ExampleForCallByValueAndCallByRef/Program.cs
+++ ExampleForCallByValueAndCallByRef/ExampleForCallByValueAndCallByRef/Program.cs
@@ -9,8 +9,20 @@ namespace ExampleForCallByValueAndCallByRef
     // Program to demo Call By Value
     class Program
     {
+        // User defined function
+        public void Show(int val)
+        {
+            val *= val; // Manipulating value
+            Console.WriteLine("Value inside the show function: " + val); // No return statement
+        }
+
         static void Main(string[] args)
         {
+            int val = 50;
+            Program program = new Program();
+            Console.WriteLine("Value before calling the function " + val);
+            program.Show(val); // Calling Function by passing value
+            Console.WriteLine("Value after caling the function " + val);
         }
     }
 }
