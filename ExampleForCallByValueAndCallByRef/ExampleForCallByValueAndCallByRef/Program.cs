8d74c0ba9d0d1bdb3ebe881d48fbe2981f3fa588
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-22 21:48:36 -0400

    Example for Call By Reference - completed

--- ExampleForCallByValueAndCallByRef/ExampleForCallByValueAndCallByRef/Program.cs
+++ ExampleForCallByValueAndCallByRef/ExampleForCallByValueAndCallByRef/Program.cs
@@ -29,9 +29,19 @@ namespace ExampleForCallByValueAndCallByRef
     // Program to demo Call By Reference
     class Program
     {
+        // User defined function
+        public void Show(ref int val)
+        {
+            val *= val; // Manipulating value
+            Console.WriteLine("Value inside the show function: " + val); // No return statement
+        }
         static void Main(string[] args)
         {
-            
+            int val = 50;
+            Program program = new Program(); // creating object
+            Console.WriteLine("Value before calling the function " + val);
+            program.Show(ref val); // Calling Function by passing value
+            Console.WriteLine("Value after caling the function " + val);
         }
     }
 }
