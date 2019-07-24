9ac16c64988e7ba4097b205a8e75d8226ca487be
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-23 21:26:21 -0400

    Example-2 to demo csharp out which returns multiple values - completed

--- ExampleForCsharpOutParameter/ExampleForCsharpOutParameter/Program.cs
+++ ExampleForCsharpOutParameter/ExampleForCsharpOutParameter/Program.cs
@@ -29,9 +29,24 @@ namespace ExampleForCsharpOutParameter
     // Example - 2 example demonstrates that how a function can return multiple values.
     class Program
     {
+        public void Show(out int a, out int b)// Out parameter
+        {
+            int square = 5;
+            a = square;
+            b = square;
+
+            // Manipulating value
+            a *= a;
+            b *= b;
+        }            
+
         static void Main(string[] args)
         {
-            
+            int val1 = 50, val2 = 100;
+            Program program = new Program(); // Creating object
+            Console.WriteLine("Value before passing \n val1 = " + val1 + "\n val2 = " + val2);
+            program.Show(out val1, out val2); // Passing out argument
+            Console.WriteLine("Value after passing \n val1 = " + val1 + "\n val2 = " + val2);
         }
     }
 }
