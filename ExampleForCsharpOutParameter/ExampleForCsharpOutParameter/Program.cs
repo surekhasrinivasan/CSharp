3677943aa9e6749672650cab3f57f42cb6fbcf7a
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-23 20:14:50 -0400

    ExampleForCsharpOutParameter - initial commit

---
+++ ExampleForCsharpOutParameter/ExampleForCsharpOutParameter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleForCsharpOutParameter
+{
+    // Example Program for C# out parameter
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
