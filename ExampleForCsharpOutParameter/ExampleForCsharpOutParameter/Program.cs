c3127c4d039a43f91563a277959978072eca79db
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-23 20:25:27 -0400

    Example for csharp out parameter - completed

--- ExampleForCsharpOutParameter/ExampleForCsharpOutParameter/Program.cs
+++ ExampleForCsharpOutParameter/ExampleForCsharpOutParameter/Program.cs
@@ -8,10 +8,21 @@ namespace ExampleForCsharpOutParameter
 {
     // Example Program for C# out parameter
     class Program
-    {        
+    {
+        public void Show(out int val)
+        {
+            int square = 5;
+            val = square;
+            val *= val; // Manipulating value
+        }
+
         static void Main(string[] args)
         {
-
+            int val = 50;
+            Program program = new Program(); // Creating object
+            Console.WriteLine("Value before passing out variable " + val);
+            program.Show(out val); // Passing out argument
+            Console.WriteLine("Value after receiving the out variable " + val);
         }
     }
 }
