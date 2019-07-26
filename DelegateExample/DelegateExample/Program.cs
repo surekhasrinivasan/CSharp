b6aafdad06e26d7a164245cfec81dc8cc638474b
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-25 21:14:00 -0400

    Csharp Delegate Example - initial commit

---
+++ DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    // Delegate is a reference to the method. It works like function pointer in C and C++. 
+    // But it is object oriented, secured and type-safe than function pointer. 
+
+    // Program to demo Delegate Example
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
