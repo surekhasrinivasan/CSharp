64361b474bb42b4f79ae35cec49f7e337d82e7f9
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-26 22:41:04 -0400

    Anonymous function example

--- LambdaFunctionExample/LambdaFunctionExample/Program.cs
+++ LambdaFunctionExample/LambdaFunctionExample/Program.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 
 namespace LambdaFunctionExample
 {
-    // Program to demo Lambda Function example
-    class Program
-    {
-        delegate int Square(int num);
-        static void Main(string[] args)
-        {
-            Square GetSquare = x => x * x;
-            int j = GetSquare(5);
-            Console.WriteLine("Square: " + j);
-        }
-    }
+    //// Program to demo Lambda Function example
+    //class Program
+    //{
+    //    delegate int Square(int num);
+    //    static void Main(string[] args)
+    //    {
+    //        Square GetSquare = x => x * x;
+    //        int j = GetSquare(5);
+    //        Console.WriteLine("Square: " + j);
+    //    }
+    //}
+
+    // Program to demo Anonymous function Example
+
 }
