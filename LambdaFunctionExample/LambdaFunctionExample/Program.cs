f4c77926c57ae5d373426e9902a53e9374974bd7
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-26 22:22:28 -0400

    LambdaFunctionExample - initial commit

---
+++ LambdaFunctionExample/LambdaFunctionExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaFunctionExample
+{
+    // Program to demo Lambda Function example
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
