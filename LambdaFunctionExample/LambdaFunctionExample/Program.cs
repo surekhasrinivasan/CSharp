00d08bd385c6f9eb23d490ebef40b36921fcaa5f
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-26 22:26:22 -0400

    LambdaFunctionExample - completed

--- LambdaFunctionExample/LambdaFunctionExample/Program.cs
+++ LambdaFunctionExample/LambdaFunctionExample/Program.cs
@@ -9,9 +9,12 @@ namespace LambdaFunctionExample
     // Program to demo Lambda Function example
     class Program
     {
+        delegate int Square(int num);
         static void Main(string[] args)
         {
-
+            Square GetSquare = x => x * x;
+            int j = GetSquare(5);
+            Console.WriteLine("Square: " + j);
         }
     }
 }
