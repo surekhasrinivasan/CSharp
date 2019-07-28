38c6d102d213c770f56324d4214e8b12421780a0
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-27 22:41:44 -0400

    Simple Enum Example - completed

--- EnumExample/EnumExample/Program.cs
+++ EnumExample/EnumExample/Program.cs
@@ -9,8 +9,15 @@ namespace EnumExample
     // Program to demo a simple Enum Example
     class Program
     {
+        public enum Season { WINTER, SPRING, SUMMER, FALL }
+
+
         static void Main(string[] args)
         {
+            int x = (int)Season.WINTER;
+            int y = (int)Season.SUMMER;
+            Console.WriteLine("WINTER = {0}", x);
+            Console.WriteLine("SUMMER = {0}", y);
         }
     }
 }
