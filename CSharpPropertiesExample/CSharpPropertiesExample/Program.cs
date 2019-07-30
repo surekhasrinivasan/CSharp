8a21cd403fac56cf80fbc62824a00215fbe156fa
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-29 21:25:30 -0400

    declare Employee class which has name C# properties

--- CSharpPropertiesExample/CSharpPropertiesExample/Program.cs
+++ CSharpPropertiesExample/CSharpPropertiesExample/Program.cs
@@ -7,10 +7,32 @@ using System.Threading.Tasks;
 namespace CSharpPropertiesExample
 {
     // Program to demo Example for C# Properties 
+
+    // C# Properites doesn't have storage location. 
+    // C# Properites are extension of fields and accessed like fields. 
+    // The Properties have accessors that are used to set, get or compute their values.
+    public class Employee
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
