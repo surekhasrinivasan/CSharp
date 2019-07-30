6f93c9399138189bbe0b11bd13fc4041f62fa1a6
surekhasrinivasan <surekhasrao@gmail.com>
2019-07-29 21:16:46 -0400

    CSharpPropertiesExample - initial commit

---
+++ CSharpPropertiesExample/CSharpPropertiesExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPropertiesExample
+{
+    // Program to demo Example for C# Properties 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
